bfa85fbafadc3f8f2d55c336fe8575c2df5fdb81
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-04-04 15:03:10 +0300

    Для нового вывода на экран начинаем писать так

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        try {
-            FileInputStream fis = new FileInputStream(new File("data/test.txt"));
-            for(;;){
-                try {
-                    int code = fis.read();
-                    if(code < 0){
-                        break;
-                    }
-                    char ch = (char) code;
-                    System.out.print(ch);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+       Files.readAllLines(Path.of("data/test.txt"));
 
     }
 
