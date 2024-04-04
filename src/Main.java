ba3d856dafbe32e489c95d9eed1e5d81e37a806d
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-04-04 10:01:21 +0300

    С файлами можно работать через Date

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+import java.io.File;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("D:\\Projects\\git\\Lesson 28 Files\\data\\test.txt");
+        Date date = new Date(file.lastModified());
+        System.out.println(date);
+    }
 }
