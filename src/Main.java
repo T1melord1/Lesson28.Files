1d92d75ec18b8f76b49c13900b2d0618b2e49170
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-04-04 14:45:14 +0300

    Создание test.txt в test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("D:\\Projects\\git\\Lesson 28 Files\\data\\test");
-        file.mkdir();
+        File file = new File("D:\\Projects\\git\\Lesson 28 Files\\data\\test\\test.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void print(File file) {
