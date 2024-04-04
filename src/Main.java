854fb1c9cb2b761e812ddb9392b8eef76a2e33ee
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-04-04 10:08:50 +0300

    Проверка папка это или файл

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         File file = new File("D:\\Projects\\git\\Lesson 28 Files\\data\\test.txt");
-        Date date = new Date(file.lastModified());
-        System.out.println(date);
+        System.out.println(file.isFile());
+        System.out.println(file.isDirectory());
+
     }
 }
