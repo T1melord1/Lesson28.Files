465c6ffed1357223aa16db249bb05fbcc7f3e062
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-04-04 14:58:11 +0300

    Старый путь вывода текста из файла на экран

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,30 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("data/test.txt");
-        System.out.println(file.length());
 
+        try {
+            FileInputStream fis = new FileInputStream(new File("data/test.txt"));
+            for(;;){
+                try {
+                    int code = fis.read();
+                    if(code < 0){
+                        break;
+                    }
+                    char ch = (char) code;
+                    System.out.print(ch);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
 
-    public static void print(File file) {
-        if (file.isDirectory()) {
-            Arrays.stream(file.listFiles()).forEach(f -> print(f));
-        }
-        if (file.isFile()) {
-            System.out.println(file.getAbsolutePath());
-        }
     }
 
 }
