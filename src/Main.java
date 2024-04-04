9e7b76605f54bf53b4752705b90006b29a31ea6f
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-04-04 15:11:17 +0300

    Запись 100 строк в файл test.txt

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,20 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+        List<String> strings = new ArrayList<>();
+        for (int i = 0; i <= 100 ; i++) {
+            strings.add(String.valueOf(i));
+        }
+
         try {
-            List<String> strings = Files.readAllLines(Path.of("data/test.txt"));
-            strings.forEach(System.out::println);
+            Files.write(Path.of("data/test.txt"), strings);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
