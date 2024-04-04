9c49b06d342e239549be9f337265cdea283b7984
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-04-04 15:15:50 +0300

    Копирование файла test.txt в папку data с такими же данными внутри только другое название test2.txt

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            Files.copy(Path.of("data/test.txt"), Path.of("data/test/"));
+            Files.copy(Path.of("data/test.txt"), Path.of("data/test2.txt"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
